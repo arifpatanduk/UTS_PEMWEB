ea28275cc5d9144c7ed0f8785ab8ed3249af27f1
arifpatanduk <arifpatanduk2@gmail.com>
2020-05-16 00:01:12 +0700

    test

--- login.php
+++ login.php
@@ -17,14 +17,13 @@
     <body class="text-center">
         <form class="form-signin">
             <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 
             <label for="inputName" class="sr-only">Nama</label>
             <input type="text" id="inputName" class="form-control" placeholder="Masukkan Nama" required autofocus>
             <label for="inputEmail" class="sr-only">Email</label>
             <input type="email" id="inputEmail" class="form-control" placeholder="Masukkan Email" required>
             
-            <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Sign in</button>
+            <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">SUBMIT</button>
             <p class="mt-5 mb-3 text-muted">&copy; Arif Wiranata Patanduk</p>
           </form>
         
