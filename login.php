e816e9a9c1434b501fab9e55d0c607d8ef3b38a9
arifpatanduk <arifpatanduk2@gmail.com>
2020-05-15 23:52:00 +0700

    file login

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Mathematics Game</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        
+        <link href="css/signin.css" rel="stylesheet">
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css'>
+    </head>
+
+    <body class="text-center">
+        <form class="form-signin">
+            <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+
+            <label for="inputName" class="sr-only">Nama</label>
+            <input type="text" id="inputName" class="form-control" placeholder="Masukkan Nama" required autofocus>
+            <label for="inputEmail" class="sr-only">Email</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Masukkan Email" required>
+            
+            <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; Arif Wiranata Patanduk</p>
+          </form>
+        
+    </body>
+
+</html>
