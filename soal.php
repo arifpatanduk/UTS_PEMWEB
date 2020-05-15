bf00e54b381db9f5b661f42cc2aa916116402f25
arifpatanduk <arifpatanduk2@gmail.com>
2020-05-16 00:42:52 +0700

    page soal

---
+++ soal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mathematics Game</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>  
+
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header">
+                Hello <strong>ARIF</strong> tetap semangat ya! You can do the best! 
+                <br>
+                <span class="badge badge-danger">LIVES : 5</span>
+                <span class="badge badge-info">SCORE : 5</span>
+            </div>
+            <div class="card-body">
+                <h4>Berapakah 5 + 7 ?</h4>
+                <form class="form-inline mt-2" method="POST" action="">
+                    <input type="number" class="form-control mb-2 mr-sm-2" id="jawab" placeholder="Jawaban...." name="email" required autofocus>
+                    <button type="submit" name="submit" class="btn btn-primary mb-2">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
